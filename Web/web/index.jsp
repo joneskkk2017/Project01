6fcf63b7fdfea453e51ede583dc9b23e3d0cc0c5
joneskkk2017 <610447598@qq.com>
2019-03-05 22:07:53 +0800

    Initial commit

---
+++ Web/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jones
+  Date: 2019-03-04
+  Time: 12:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>测试</title>
+  </head>
+  <body>
+  Idea
+  </body>
+</html>
---
+++ src/HelloWorld.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+    //测试用
+    public static void main(String[] args){
+
+
+        System.out.println("HelloWorld!");
+
+        ArrayList list = new ArrayList();
+        Date date = new Date();
+        System.out.println();
+
+        System.out.println("HelloWorld.main");
+
+        for (int i = 0; i < 100; i++) {
+            
+        }
+        for (Object o : list) {
+            
+        }
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+        }
+        for (Object o : list) {
+            
+        }
+        if (date == null) {
+            
+        }
+        if (date != null) {
+            
+        }
+    }
+
+
+}
